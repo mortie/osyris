dc27314bdb3491c09666da3942958bdec4bfa25a
Martin Dørum <martid0311@gmail.com>
2022-03-30 14:19:42 +0200

    better CLI

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,5 @@
 use std::rc::Rc;
+use std::fmt;
 
 #[derive(Debug)]
 pub enum Expression {
@@ -7,4 +8,52 @@ pub enum Expression {
     Lookup(String),
     Call(Vec<Expression>),
     Quote(Rc<Vec<Expression>>),
+}
+
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expression::String(s) => {
+                write!(f, "\"")?;
+                for ch in s.chars() {
+                    if ch == '\\' {
+                        write!(f, "\\\\")?;
+                    } else if ch == '"' {
+                        write!(f, "\\\"")?;
+                    } else {
+                        write!(f, "{}", ch)?;
+                    }
+                }
+                write!(f, "\"")
+            }
+            Expression::Number(num) => write!(f, "{}", num),
+            Expression::Lookup(name) => write!(f, "{}", name),
+            Expression::Call(exprs) => {
+                write!(f, "(")?;
+                let mut first = true;
+                for expr in exprs {
+                    if !first {
+                        write!(f, " ")?;
+                    }
+                    first = false;
+
+                    write!(f, "{}", expr)?;
+                }
+                write!(f, ")")
+            }
+            Expression::Quote(exprs) => {
+                write!(f, "'(")?;
+                let mut first = true;
+                for expr in exprs.iter() {
+                    if !first {
+                        write!(f, " ")?;
+                    }
+                    first = false;
+
+                    write!(f, "{}", expr)?;
+                }
+                write!(f, ")")
+            }
+        }
+    }
 }
--- src/bin/osyris.rs
+++ src/bin/osyris.rs
@@ -3,23 +3,50 @@ use std::cell::RefCell;
 use std::env;
 use std::fs;
 use std::rc::Rc;
+use std::process;
+
+fn usage(argv0: &String) {
+    println!("Usage: {} [options] <path>", argv0);
+    println!("Options:");
+    println!("  --help, -h:  Show this help text");
+    println!("  --print-ast: Print the syntax tree instead of executing");
+}
 
 fn main() {
     let mut args = env::args();
-    args.next().unwrap();
+    let argv0 = args.next().unwrap();
 
-    let path = match args.next() {
+    let mut path: Option<String> = None;
+    let mut print_ast = false;
+    let mut dashes = false;
+    while let Some(arg) = args.next() {
+        if !dashes && (arg == "--help" || arg == "-h") {
+            usage(&argv0);
+            return;
+        } else if !dashes && (arg == "--print-ast") {
+            print_ast = true;
+        } else if !dashes && arg == "--" {
+            dashes = true;
+        } else if path.is_none() {
+            path = Some(arg);
+        } else {
+            eprintln!("Unexpected arguemnt: {}", arg);
+            process::exit(1);
+        }
+    }
+
+    let path = match path {
         Some(path) => path,
         None => {
-            println!("Need argument");
-            return;
+            usage(&argv0);
+            process::exit(1);
         }
     };
 
     let string = match fs::read_to_string(&path) {
         Ok(string) => string,
         Err(err) => {
-            println!("{}: {}", path, err);
+            eprintln!("{}: {}", path, err);
             return;
         }
     };
@@ -38,17 +65,21 @@ fn main() {
                 None => break,
             },
             Err(err) => {
-                println!("Parse error: {}:{}: {}", err.line, err.col, err.msg);
-                return;
+                eprintln!("Parse error: {}:{}: {}", err.line, err.col, err.msg);
+                process::exit(1);
             }
         };
 
-        match eval::eval(&expr, &scope) {
-            Err(err) => {
-                println!("Error: {}", err);
-                return;
+        if print_ast {
+            println!("{}", expr);
+        } else {
+            match eval::eval(&expr, &scope) {
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                    process::exit(1);
+                }
+                _ => (),
             }
-            _ => (),
         }
     }
 }
